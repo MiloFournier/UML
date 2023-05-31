3cdbd884d4707a53ce87bca96fc56cfe3daf19f0
MiloFournier <milo.fournier@gmail.com>
2023-05-31 15:33:29 +0200

    Services -> verifEtatCapteur

--- UML/bin/model/Services.cpp
+++ UML/bin/model/Services.cpp
@@ -3,23 +3,62 @@
 //
     #include "Services.h"
     #include <stdbool.h>
+    #include <cmath>
 
     using namespace std;
 
     bool Service::verifierEtatCapteur(Capteur capteurParam) {
-
         double distanceDeVerification = 200.0; // en km
         int limiteDeTemps = 1; // même jour
-        double limiteDeMesure = 0.0; // valeur limite de différence entre les mesures
+        double limiteDeMesure = 5.0; // valeur limite de différence entre les mesures
         int compteurErreurs = 0;
         int compteur = 0;
 
+        Database d = new Database();
+        unordered_map<string, Capteur> listeDesCapteurs = d.getMCapteurs();
         // Parcours de tous les capteurs
         for (const auto& capteurTest : listeDesCapteurs) {
         // Vérification de la distance entre capteurParam et capteurTest
             if (calculDistance(capteurParam, capteurTest) < distanceDeVerification) {
                 // Vérification de la date des mesures
-                if (comparerDates(capteurParam.getMesures(), capteurTest.getMesures()) <= limiteDeTemps) {
+                for(const auto &i : capteurParam.getLMesure_O3()) {
+                    for(const auto &j : capteurTest.getLMesures_O3()) {
+                        if(i->first == j->first) {
+                            if(abs(i->second.getValeur() - j->second.getValeur()) > limiteDeMesure) {
+                                compteurErreurs++;
+                            }
+                        }
+                    }
+                }
+                for(const auto &i : capteurParam.getLMesure_NO2()) {
+                    for(const auto &j : capteurTest.getLMesures_NO2()) {
+                        if(i->first == j->first) {
+                            if(abs(i->second.getValeur() - j->second.getValeur()) > limiteDeMesure) {
+                                compteurErreurs++;
+                            }
+                        }
+                    }
+                }
+                for(const auto &i : capteurParam.getLMesure_SO2()) {
+                    for(const auto &j : capteurTest.getLMesures_SO2()) {
+                        if(i->first == j->first) {
+                            if(abs(i->second.getValeur() - j->second.getValeur()) > limiteDeMesure) {
+                                compteurErreurs++;
+                            }
+                        }
+                    }
+                }
+                for(const auto &i : capteurParam.getLMesure_PM10()) {
+                    for(const auto &j : capteurTest.getLMesures_PM10()) {
+                        if(i->first == j->first) {
+                            if(abs(i->second.getValeur() - j->second.getValeur()) > limiteDeMesure) {
+                                compteurErreurs++;
+                            }
+                        }
+                    }
+                }
+                compteur += 4;
+                /*if (comparerDates(capteurParam.getMesures(), capteurTest.getMesures()) <= limiteDeTemps) {
                     // Parcours des gaz pour comparer les mesures
                     for (const auto& gaz : listeDesGaz) {
                         double difference = differenceValeurs(capteurParam.getMesure(gaz), capteurTest.getMesure(gaz));
@@ -28,18 +67,15 @@
                         }
                     }
                     compteur += 4; // Ajouter le nombre de mesures comparées (4 gaz)
-                }
+                }*/
             }
         }
-
         double tauxErreur = (compteurErreurs / compteur) * 100.0;
         bool dysfonctionnel = false;
-
         if (tauxErreur > 70.0) {
             dysfonctionnel = true;
         }
-
-        return !dysfonctionnel;
+        return dysfonctionnel;
     }
 
 
